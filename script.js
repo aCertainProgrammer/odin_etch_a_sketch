566dc79dbf5918079600c9f24cf640d3989e00d1
Rycer <szymkris5@gmail.com>
2024-04-02 09:20:08 +0200

    base project done

--- script.js
+++ script.js
@@ -6,22 +6,6 @@ function changeColor(event){
     event.target.style.backgroundColor = "pink";
 };
 
-grid_container.style.backgroundColor = "red";
-grid_container.style.height = "960px";
-grid_container.style.width = "960px";
-grid_container.style.maxWidth = "960px";
-grid_container.style.maxHeight = "960px";
-grid_container.style.display = "flex";
-grid_container.style.flexWrap = "wrap";
-
-grid_element.style.height = "60px";
-grid_element.style.width = "60px";
-grid_element.style.backgroundColor = "blue";
-grid_element.style.flexGrow = "0";
-grid_element.style.flexShrink = "0";
-grid_element.style.flexBasis = "auto";
-grid_element.style.pointerEvents = "auto";
-
 for (let i = 0; i < 16 * 16 ; i++) {
     grid_container.appendChild(grid_element.cloneNode(true)); 
 }
@@ -38,8 +22,8 @@ function resetGrid(){
         gridArray[i].remove();
     }
     grid_border_size = reset_data.value;
-    grid_element.style.height = (960/grid_border_size);
-    grid_element.style.width = (960/grid_border_size);
+    grid_element.style.height = (960/grid_border_size) + "px";
+    grid_element.style.width = (960/grid_border_size) + "px";
     for (let i = 0; i < grid_border_size * grid_border_size; i++) {
         grid_container.appendChild(grid_element.cloneNode(true)); 
     }
